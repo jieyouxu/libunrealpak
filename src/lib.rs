6a3ab1657d2737b2f00cb4a51d0a3452b210d12c
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2023-01-22 15:12:49 +0800

    Only retain detailed version

--- src/lib.rs
+++ src/lib.rs
@@ -40,31 +40,75 @@ pub struct FooterCommon {
     pub index_hash: [u8; 20],
 }
 
-/// Version of the Unreal `.pak` archive. Adapted from [`unpak`]'s `VersionMajor`.
-///
-/// [`unpak`]: https://github.com/trumank/unpak/blob/master/src/lib.rs
+/// Major and minor versions of the Unreal `.pak` archive format.
 #[derive(Debug, Copy, Clone, PartialEq, PartialOrd)]
 pub enum Version {
-    /// Initial version
-    Initial = 1,
-    /// Timestamps removed
-    NoTimestamps = 2,
-    /// Compression and encryption support
-    CompressionEncryption = 3,
-    /// Index encryption support
-    IndexEncryption = 4,
-    /// Offsets relative to header
-    RelativeChunkOffsets = 5,
-    /// Record deletion support
-    DeleteRecords = 6,
-    /// Include key GUID
-    EncryptionKeyGUID = 7,
-    /// Compression names included
-    FNameBasedCompression = 8,
-    /// TODO: what's the difference between V8A and V8B?
-    FrozenIndex = 9,
-    /// Frozen index byte included
-    PathHashIndex = 10,
-    /// TODO: find out what this version changed
-    Fnv64BugFix = 11,
+    V1,
+    V2,
+    V3,
+    V4,
+    V5,
+    V6,
+    V7,
+    V8A,
+    V8B,
+    V9,
+    V10,
+    V11,
+}
+
+impl Version {
+    pub fn major_version(self) -> u32 {
+        match self {
+            Self::V1 => 1,
+            Self::V2 => 2,
+            Self::V3 => 3,
+            Self::V4 => 4,
+            Self::V5 => 5,
+            Self::V6 => 6,
+            Self::V7 => 7,
+            Self::V8A => 8,
+            Self::V8B => 8,
+            Self::V9 => 9,
+            Self::V10 => 10,
+            Self::V11 => 11,
+        }
+    }
+
+    pub fn footer_size(self) -> u64 {
+        // Taken verbatim from
+        // <https://github.com/trumank/unpak/blob/de69a98d34d902418916d15877f595f161074147/src/lib.rs#L57>.
+        let mut size = {
+            // Size of common properties across all versions:
+            // magic (4) + version (4) + offset (8) + size (8) + hash (20)
+            4 + 4 + 8 + 8 + 20
+        };
+
+        if self.major_version() >= 4 {
+            // encryption uuid (16)
+            size += 16;
+        }
+
+        if self.major_version() >= 7 {
+            // is encrypted (1)
+            size += 1;
+        }
+
+        if self.major_version() == 9 {
+            // is frozen (1)
+            size += 1;
+        }
+
+        if self >= Version::V8A {
+            // compression names (128)
+            size += 128;
+        }
+
+        if self >= Version::V8B {
+            // additional compression name (+32)
+            size += 32;
+        }
+
+        size
+    }
 }

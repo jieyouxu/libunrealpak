bbcc1ce567bcb649a6b02f0c09a0f9c6f443c362
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2023-01-28 11:00:47 +0800

    Update Index definition

--- src/index.rs
+++ src/index.rs
@@ -1,18 +1,12 @@
-use crate::{Compression, Hash, PathHashIndex, FullDirectoryIndex, Record};
+use crate::{PathHashIndex, FullDirectoryIndex, Record};
 use std::collections::BTreeMap;
 
 #[derive(Debug, PartialEq)]
-pub(crate) struct LegacyIndex {
+pub(crate) struct Index {
     pub(crate) mount_point: String,
-    pub(crate) entries: Vec<Record>,
-}
-
-#[derive(Debug, PartialEq)]
-pub(crate) struct NewIndex {
-    pub(crate) mount_point: String,
-    pub(crate) entry_count: u32,
-    pub(crate) path_hash_seed: u64,
+    pub(crate) record_count: u32,
+    pub(crate) path_hash_seed: Option<u64>,
     pub(crate) path_hash_index: Option<PathHashIndex>,
     pub(crate) full_directory_index: Option<FullDirectoryIndex>,
-    pub(crate) entries: BTreeMap<String, Record>,
+    pub(crate) records: BTreeMap<String, Record>,
 }
